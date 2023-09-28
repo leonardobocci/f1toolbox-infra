e10cf1af3f1c0ca22611c739c5ea51427c1348d2
Leonardo Bocci <leonardobocci99@hotmail.com>
2023-09-28 16:43:43 +0200

    Refactored back into 1 file

--- infra/modules/gke/main.tf
+++ infra/modules/gke/main.tf
@@ -12,3 +12,30 @@ resource "google_container_cluster" "gke_orchestration_ingestion_cluster" {
 
   depends_on = [google_project_service.enable_gke]
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.gke_orchestration_ingestion_cluster.endpoint
+    cluster_ca_certificate = base64decode(google_container_cluster.gke_orchestration_ingestion_cluster.master_auth[0].cluster_ca_certificate)
+    client_certificate     = base64decode(google_container_cluster.gke_orchestration_ingestion_cluster.master_auth[0].client_certificate)
+    client_key             = base64decode(google_container_cluster.gke_orchestration_ingestion_cluster.master_auth[0].client_key)
+  }
+}
+
+resource "helm_release" "airbyte" {
+  name       = "airbyte"
+  repository = "https://airbytehq.github.io/helm-charts"
+  chart      = "airbyte"
+  version    = "0.49.1"
+
+  depends_on = [ google_container_cluster.gke_orchestration_ingestion_cluster ]
+}
+
+resource "helm_release" "dagster" {
+  name       = "dagster"
+  repository = "https://dagster-io.github.io/helm"
+  chart      = "dagster"
+  version    = "1.4.16"
+
+  depends_on = [ helm_release.airbyte ]
+}
