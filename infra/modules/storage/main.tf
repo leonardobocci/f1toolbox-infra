fd196d0bad46681e472cd6fd3988d2250222f0c0
Leonardo Bocci <leonardobocci99@hotmail.com>
2023-09-28 15:33:49 +0200

    Renamed Buckets

--- infra/modules/storage/main.tf
+++ infra/modules/storage/main.tf
@@ -2,7 +2,7 @@ resource "google_project_service" "enable_storage" {
     service = "storage-api.googleapis.com"
 }
 
-resource "google_storage_bucket" "bronze_bucket" {
+resource "google_storage_bucket" "landing_bucket" {
   name = "bronze-landing-zone"
   location = var.bucket_region
   force_destroy = true
@@ -10,10 +10,10 @@ resource "google_storage_bucket" "bronze_bucket" {
   depends_on = [ google_project_service.enable_storage ]
 }
 
-resource "google_storage_bucket" "silver_bucket" {
+resource "google_storage_bucket" "transformed_bucket" {
   name = "silver-transformed-zone"
   location = var.bucket_region
   force_destroy = true
 
-  depends_on = [ google_project_service.enable_storage ]
+  depends_on = [ google_storage_bucket.bronze_bucket ]
 }
