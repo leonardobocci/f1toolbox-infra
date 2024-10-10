c3949cf664ee3730d748d7101e610979e601dab1
leonardobocci <leonardobocci99@hotmail.com>
2024-10-10 14:33:32 +0200

    enable iam api

--- infra/modules/auth/main.tf
+++ infra/modules/auth/main.tf
@@ -1,5 +1,11 @@
+resource "google_project_service" "enable_iam" {
+  service = "iam.googleapis.com"
+}
+
 resource "google_service_account" "airbyte_service_account" {
   account_id   = "airbyte"
   display_name = "Airbyte Service Account"
   project      = var.project
+
+  depends_on = [google_project_service.enable_iam]
 }
