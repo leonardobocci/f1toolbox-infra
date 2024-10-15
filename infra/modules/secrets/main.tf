4bd01322466c5ef5feb58eb9fe6d171d234c4d6f
leonardobocci <leonardobocci99@hotmail.com>
2024-10-15 12:09:08 +0200

    airbyte secret manager admin

--- infra/modules/secrets/main.tf
+++ infra/modules/secrets/main.tf
@@ -3,33 +3,9 @@ resource "google_project_service" "enable_secret_manager" {
     disable_on_destroy = false
 }
 
-resource "google_project_iam_member" "secretAccessor" {
+resource "google_project_iam_member" "secretAdmin" {
   project = var.project
-  role = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:${var.airbyte_auth_service_account_email}"
-
-  depends_on = [ google_project_service.enable_secret_manager ]
-}
-
-resource "google_project_iam_member" "secretVersionAdder" {
-  project = var.project
-  role = "roles/secretmanager.secretVersionAdder"
-  member  = "serviceAccount:${var.airbyte_auth_service_account_email}"
-
-  depends_on = [ google_project_service.enable_secret_manager ]
-}
-
-resource "google_project_iam_member" "secretVersionManager" {
-  project = var.project
-  role = "roles/secretmanager.secretVersionManager"
-  member  = "serviceAccount:${var.airbyte_auth_service_account_email}"
-
-  depends_on = [ google_project_service.enable_secret_manager ]
-}
-
-resource "google_project_iam_member" "secretViewer" {
-  project = var.project
-  role = "roles/secretmanager.viewer"
+  role = "roles/secretmanager.admin"
   member  = "serviceAccount:${var.airbyte_auth_service_account_email}"
 
   depends_on = [ google_project_service.enable_secret_manager ]
