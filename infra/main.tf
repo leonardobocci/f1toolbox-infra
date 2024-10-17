bb0864d3bd1d4df9e60add323d98b25287e7595f
leonardobocci <116392117+leonardobocci@users.noreply.github.com>
2024-10-17 14:40:32 +0000

    terraform fmt

--- infra/main.tf
+++ infra/main.tf
@@ -1,31 +1,31 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.7.0"
     }
   }
 }
 
 provider "google" {
-    credentials = var.GOOGLE_CREDENTIALS
-    project = var.project
-    region = var.region
-    zone = var.zone
+  credentials = var.GOOGLE_CREDENTIALS
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
 }
 
 module "storage" {
-  source = "./modules/storage"
-  project = var.project
-  airbyte_auth_service_account_email = module.auth.airbyte_service_account_email
-  terraform_service_account_email = var.terraform_service_account_email
-  project_user_gmail = var.project_user_gmail
+  source                                      = "./modules/storage"
+  project                                     = var.project
+  airbyte_auth_service_account_email          = module.auth.airbyte_service_account_email
+  terraform_service_account_email             = var.terraform_service_account_email
+  project_user_gmail                          = var.project_user_gmail
   workload_identity_gke_service_account_email = module.auth.workload_identity_gke_service_account.email
 }
 
 module "secrets" {
-  source = "./modules/secrets"
-  project = var.project
+  source                             = "./modules/secrets"
+  project                            = var.project
   airbyte_auth_service_account_email = module.auth.airbyte_service_account_email
 }
 
@@ -34,13 +34,13 @@ module "gke" {
 }
 
 module "auth" {
-  source = "./modules/auth"
+  source  = "./modules/auth"
   project = var.project
 }
 
 module "bigquery" {
-  source = "./modules/bigquery"
-  project = var.project
+  source                             = "./modules/bigquery"
+  project                            = var.project
   airbyte_auth_service_account_email = module.auth.airbyte_service_account_email
-  terraform_service_account_email = var.terraform_service_account_email
+  terraform_service_account_email    = var.terraform_service_account_email
 }
