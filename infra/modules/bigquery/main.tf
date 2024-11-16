556de388af6a7da57d910e75add8ba3a2d6e063c
leonardobocci <leonardobocci99@hotmail.com>
2024-11-16 14:03:24 +0100

    rollback usage of multiple datasets

--- infra/modules/bigquery/main.tf
+++ infra/modules/bigquery/main.tf
@@ -3,86 +3,8 @@ resource "google_project_service" "enable_iam" {
   disable_on_destroy = false
 }
 
-resource "google_bigquery_dataset" "f1toolbox_core_bronze_dataset" {
-  dataset_id                 = "f1toolbox_core_bronze"
-  friendly_name              = "f1toolbox"
-  description                = "Core data for f1 toolbox"
-  location                   = var.bigquery_location
-  delete_contents_on_destroy = true
-
-  access {
-    role          = "WRITER"
-    special_group = "projectWriters"
-  }
-  #required explicit owner
-  access {
-    role          = "OWNER"
-    user_by_email = var.terraform_service_account_email
-  }
-  access {
-    role          = "OWNER"
-    special_group = "projectOwners"
-  }
-  access {
-    role          = "READER"
-    special_group = "projectReaders"
-  }
-}
-
-resource "google_bigquery_dataset" "f1toolbox_core_silver_dataset" {
-  dataset_id                 = "f1toolbox_core_silver"
-  friendly_name              = "f1toolbox"
-  description                = "Core data for f1 toolbox"
-  location                   = var.bigquery_location
-  delete_contents_on_destroy = true
-
-  access {
-    role          = "WRITER"
-    special_group = "projectWriters"
-  }
-  #required explicit owner
-  access {
-    role          = "OWNER"
-    user_by_email = var.terraform_service_account_email
-  }
-  access {
-    role          = "OWNER"
-    special_group = "projectOwners"
-  }
-  access {
-    role          = "READER"
-    special_group = "projectReaders"
-  }
-}
-
-resource "google_bigquery_dataset" "f1toolbox_core_gold_dataset" {
-  dataset_id                 = "f1toolbox_core_gold"
-  friendly_name              = "f1toolbox"
-  description                = "Core data for f1 toolbox"
-  location                   = var.bigquery_location
-  delete_contents_on_destroy = true
-
-  access {
-    role          = "WRITER"
-    special_group = "projectWriters"
-  }
-  #required explicit owner
-  access {
-    role          = "OWNER"
-    user_by_email = var.terraform_service_account_email
-  }
-  access {
-    role          = "OWNER"
-    special_group = "projectOwners"
-  }
-  access {
-    role          = "READER"
-    special_group = "projectReaders"
-  }
-}
-
-resource "google_bigquery_dataset" "f1toolbox_core_marts_dataset" {
-  dataset_id                 = "f1toolbox_core_marts"
+resource "google_bigquery_dataset" "f1toolbox_core_dataset" {
+  dataset_id                 = "f1toolbox_core"
   friendly_name              = "f1toolbox"
   description                = "Core data for f1 toolbox"
   location                   = var.bigquery_location
